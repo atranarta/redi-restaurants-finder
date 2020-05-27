ec602dbdbb3bb0977b5ceaee8cb023ba1133c08c
Ira Gnativ <irina.gnativ@gmail.com>
2020-05-27 22:47:34 +0200

    Refactored SearchBar component to a functional one

--- src/components/SearchFilterBar/SearchBar/index.js
+++ src/components/SearchFilterBar/SearchBar/index.js
@@ -2,23 +2,19 @@ import React from "react";
 
 import "./SearchBar.scss";
 
-export class SearchBar extends React.Component {
-  render() {
-    return (
-      // <div className="searchBar">
-      <div className="searchField">
-        <input
-          className="search"
-          placeholder="Enter restaurant name here..."
-          type="text"
-        />
-        <button>
-          <i class="fa fa-search"></i>
-        </button>
-      </div>
-      // </div>
-    );
-  }
-}
+const SearchBar = () => {
+  return (
+    <div className="searchField">
+      <input
+        className="search"
+        placeholder="Enter restaurant name here..."
+        type="text"
+      />
+      <button>
+        <i class="fa fa-search"></i>
+      </button>
+    </div>
+  );
+};
 
 export default SearchBar;
