66d3d12a8e6d619e72099efb740e239cbfd56b5f
atranarta <radyuk.tatyana@gmail.com>
2020-05-28 22:12:39 +0200

    Fixed styles nesting

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -6,7 +6,7 @@ import "./List.scss";
 import Placeholder from "../../assets/images/Raspberry_in_Bowl.jpg";
 
 const getRatingColor = (rating) => {
-  if (rating <= 2.3 ) {
+  if (rating <= 2.3) {
     return `#8B0000`
   } else if (rating <= 2.8) {
     return `#FF6347`
@@ -26,41 +26,41 @@ const apiLink =
 // Component, performing fetching data from the API and displaying the total list of restaurants,
 // including their names and addresses
 const List = () => {
-const [rests, setRests] = useState([]);
-useEffect(() => {
-  axios
-    .get(apiLink)
-    .then((res) => {
-      setRests(res.data.results);
-    })
-    .catch((error) => console.log(error));
-}, []);
-return (
-  <main>
-    {rests.map((rest) => (
-      <li className="ListItem" key={rest.id}>
-        <div className="textbox">
-          <h2>{rest.name}</h2>
-          <p className="address">{rest.formatted_address}</p>
-          <div className="details">
-            <p className="price">{"$".repeat(rest.price_level)}</p>
-            <p className="rating">
-              Rating: <span className="rating-number" style={{ backgroundColor: getRatingColor(rest.rating) }}>{rest.rating}</span>
-            </p>
-            <p className="isOpen">
-              {rest.opening_hours.open_now ? (
-                <span className="open">Open</span>
-              ) : (
-                  <span className="closed">Closed</span>
-                )}
-            </p>
+  const [rests, setRests] = useState([]);
+  useEffect(() => {
+    axios
+      .get(apiLink)
+      .then((res) => {
+        setRests(res.data.results);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+  return (
+    <main>
+      {rests.map((rest) => (
+        <li className="ListItem" key={rest.id}>
+          <div className="textbox">
+            <h2>{rest.name}</h2>
+            <p className="address">{rest.formatted_address}</p>
+            <div className="details">
+              <p className="price">{"$".repeat(rest.price_level)}</p>
+              <p className="rating">
+                Rating: <span className="rating-number" style={{ backgroundColor: getRatingColor(rest.rating) }}>{rest.rating}</span>
+              </p>
+              <p className="isOpen">
+                {rest.opening_hours.open_now ? (
+                  <span className="open">Open</span>
+                ) : (
+                    <span className="closed">Closed</span>
+                  )}
+              </p>
+            </div>
           </div>
-        </div>
-        <img src={Placeholder} alt="restaurant" />
-      </li>
-    ))}
-  </main>
-);
+          <img src={Placeholder} alt="restaurant" />
+        </li>
+      ))}
+    </main>
+  );
 };
 
 export default List;
