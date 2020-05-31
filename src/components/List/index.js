b9707e132618be53656c567f3ff39c37a7f8b904
atranarta <radyuk.tatyana@gmail.com>
2020-05-31 21:13:06 +0200

    Added filters (without styling)

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -35,30 +35,61 @@ const List = () => {
       })
       .catch((error) => console.log(error));
   }, []);
-  return (
-    <main>
-      {rests.map((rest) => (
-        <li className="ListItem" key={rest.id}>
-          <img src={Placeholder} alt="restaurant" />
-          <div className="textbox">
-            <h2>{rest.name}</h2>
-            <p className="address">{rest.formatted_address}</p>
-            <p className="price">{"$".repeat(rest.price_level)}</p>
 
-            <div className="isOpen">
-              {rest.opening_hours.open_now ? (
-                <p className="open">Open</p>
-              ) : (
-                  <p className="closed">Closed</p>
-                )}
+  const [selectedPrice, setSelectedPrice] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const filterRestaurantsByPrice = rest => {
+    if (selectedPrice !== "" && typeof rest.price_level !== 'undefined') {
+      return rest.price_level.toString() === selectedPrice;
+    }
+
+    return true;
+  };
+
+  const filterRestaurantsByOpen = rest => {
+    if (isOpen) {
+      return rest.opening_hours.open_now;
+    }
+
+    return true;
+  };
+
+  return (
+    <>
+      <div className="filterBox">
+        <select name="price" id="price" onChange={event => setSelectedPrice(event.target.value)}>
+          <option value="">Show all</option>
+          <option value="1">$</option>
+          <option value="2">$$</option>
+          <option value="3">$$$</option>
+          <option value="4">$$$$</option>
+        </select>
+        <label for="open">Open now</label>
+        <input type="checkbox" id="open" name="open" onChange={() => setIsOpen(!isOpen)} />
+      </div>
+      <main>
+        {rests.filter(filterRestaurantsByPrice).filter(filterRestaurantsByOpen).map((rest) => (
+          <li className="ListItem" key={rest.id}>
+            <img src={Placeholder} alt="restaurant" />
+            <div className="textbox">
+              <h2>{rest.name}</h2>
+              <p className="address">{rest.formatted_address}</p>
+              <p className="price">{"$".repeat(rest.price_level)}</p>
+
+              <div className="isOpen">
+                {rest.opening_hours.open_now
+                  ? (<p className="open">Open</p>)
+                  : (<p className="closed">Closed</p>)}
+              </div>
             </div>
-          </div>
-          <p className="rating">
-            <span className="rating-number" style={{ backgroundColor: getRatingColor(rest.rating) }}>{rest.rating}</span>
-          </p>
-        </li>
-      ))}
-    </main>
+            <p className="rating">
+              <span className="rating-number" style={{ backgroundColor: getRatingColor(rest.rating) }}>{rest.rating}</span>
+            </p>
+          </li>
+        ))}
+      </main>
+    </>
   );
 };
 
