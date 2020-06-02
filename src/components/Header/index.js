a475001b8f54326b9bc91167c9b7df25a24a9dfb
Ira Gnativ <irina.gnativ@gmail.com>
2020-06-02 23:44:49 +0200

    Added home button on top right

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import homeIcon from "../../assets/images/home-24px.svg";
 
-import './Header.scss';
+import "./Header.scss";
 
 const Header = () => (
   <header>
+    <Link to="/">
+      <img className="home-icon" src={homeIcon} alt="home icon" />
+    </Link>
     <h1>Find your perfect place to eat</h1>
   </header>
-)
+);
 
 export default Header;
