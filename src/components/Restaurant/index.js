ec1ac1a413d314c9a22cee1b64b735d0f5649a0d
Ira Gnativ <irina.gnativ@gmail.com>
2020-06-03 15:21:47 +0200

    Installed OpenLayers library and added map to a Restaurant component

--- src/components/Restaurant/index.js
+++ src/components/Restaurant/index.js
@@ -5,6 +5,20 @@ import { connect } from "react-redux";
 import BackButton from "./BackButton";
 import "./Restaurant.scss";
 
+// Imports from Open Layers, in order to display map
+// import Map from "ol/Map";
+// import View from "ol/View";
+import { Tile, Vector as VectorLayer } from "ol/layer";
+import { fromLonLat } from "ol/proj";
+import { Vector as VectorSource } from "ol/source";
+import { Feature } from "ol";
+import { Point } from "ol/geom";
+import "ol/ol.css";
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+
 const Restaurant = ({ restaurants }) => {
   let { name } = useParams();
 
@@ -13,6 +27,39 @@ const Restaurant = ({ restaurants }) => {
   }
 
   const restaurant = restaurants.filter((item) => item.name === name)[0];
+
+  // Creating a map instance,
+  // and a layer of the map with a blue pin of the restaurant location, in the middle
+  const map = new Map({
+    layers: [
+      new TileLayer({
+        source: new OSM(),
+      }),
+    ],
+    target: "map",
+    view: new View({
+      center: fromLonLat([
+        rest.geometry.location.lng,
+        rest.geometry.location.lat,
+      ]),
+      zoom: 18,
+    }),
+  });
+
+  const layer = new VectorLayer({
+    source: new VectorSource({
+      features: [
+        new Feature({
+          geometry: new Point(
+            fromLonLat([rest.geometry.location.lng, rest.geometry.location.lat])
+          ),
+          // color: "red",
+        }),
+      ],
+    }),
+  });
+  map.addLayer(layer);
+
   return (
     <>
       <BackButton />
@@ -40,6 +87,9 @@ const Restaurant = ({ restaurants }) => {
               {restaurant.social.phone}, {restaurant.social.email}
             </span>
           </p>
+          <div className="map-wrapper">
+            <div id="map" className="map"></div>
+          </div>
         </div>
       </div>
     </>
