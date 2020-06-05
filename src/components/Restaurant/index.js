85d80d9931f85783e0cd1bfec0ae373db00f5034
atranarta <radyuk.tatyana@gmail.com>
2020-06-05 18:51:01 +0200

    Swapped the elements in the RestaurantCard

--- src/components/Restaurant/index.js
+++ src/components/Restaurant/index.js
@@ -15,25 +15,25 @@ const Restaurant = ({ rests }) => {
   const rest = rests.filter((item) => item.name === name)[0];
 
   return (
-    <main>
-      <div className="card">
+    <>
+      <div className="RestaurantCard">
         <img src={rest.photos[0].links[1]} alt="restaurant" />
         <div className="textbox">
           <h2>{rest.name}</h2>
           <p>{rest.formatted_address}</p>
-          <p>{rest.social.phone}</p>
-          <p>{rest.social.email}</p>
+          <p style={{ textTransform: "capitalize" }}>
+            Cuisine: {rest.cuisine}, {rest.dietaryRestrictions}
+          </p>
+          <p>Rating: {rest.rating}</p>
           <p>
             Open from {rest.opening_hours.hours.open} to{" "}
             {rest.opening_hours.hours.open}
           </p>
-          <p>Rating: {rest.rating}</p>
-          <p style={{ textTransform: "capitalize" }}>
-            Food: {rest.cuisine}, {rest.dietaryRestrictions}
-          </p>
+          <p>{rest.social.phone}</p>
+          <p>{rest.social.email}</p>
         </div>
       </div>
-    </main>
+    </>
   );
 };
 
