cbca96a46aa29a2e46099bbe909e816214b30ffa
Emre Karaca <emre.karaca@easybell.de>
2020-06-08 20:08:23 +0200

    wrap map logic inside layoutEffect

--- src/components/Restaurant/index.js
+++ src/components/Restaurant/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useLayoutEffect} from "react";
 import { useParams } from "react-router-dom";
 import { connect } from "react-redux";
 
@@ -8,7 +8,7 @@ import "./Restaurant.scss";
 // Imports from Open Layers, in order to display map
 // import Map from "ol/Map";
 // import View from "ol/View";
-import { Tile, Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorLayer } from "ol/layer";
 import { fromLonLat } from "ol/proj";
 import { Vector as VectorSource } from "ol/source";
 import { Feature } from "ol";
@@ -22,43 +22,45 @@ import OSM from "ol/source/OSM";
 const Restaurant = ({ restaurants }) => {
   let { name } = useParams();
 
+  // Creating a map instance,
+  // and a layer of the map with a blue pin of the restaurant location, in the middle
+  useLayoutEffect(() => {
+    const map = new Map({
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      target: "map",
+      view: new View({
+        center: fromLonLat([
+          restaurant.geometry.location.lng,
+          restaurant.geometry.location.lat,
+        ]),
+        zoom: 18,
+      }),
+    });
+  
+    const layer = new VectorLayer({
+      source: new VectorSource({
+        features: [
+          new Feature({
+            geometry: new Point(
+              fromLonLat([restaurant.geometry.location.lng, restaurant.geometry.location.lat])
+            ),
+            // color: "red",
+          }),
+        ],
+      }),
+    });
+    map.addLayer(layer);
+  }, [])  
+
   if (restaurants.length === 0) {
     return <>Please return to Home page...</>;
   }
 
   const restaurant = restaurants.filter((item) => item.name === name)[0];
-
-  // Creating a map instance,
-  // and a layer of the map with a blue pin of the restaurant location, in the middle
-  const map = new Map({
-    layers: [
-      new TileLayer({
-        source: new OSM(),
-      }),
-    ],
-    target: "map",
-    view: new View({
-      center: fromLonLat([
-        rest.geometry.location.lng,
-        rest.geometry.location.lat,
-      ]),
-      zoom: 18,
-    }),
-  });
-
-  const layer = new VectorLayer({
-    source: new VectorSource({
-      features: [
-        new Feature({
-          geometry: new Point(
-            fromLonLat([rest.geometry.location.lng, rest.geometry.location.lat])
-          ),
-          // color: "red",
-        }),
-      ],
-    }),
-  });
-  map.addLayer(layer);
 
   return (
     <>
